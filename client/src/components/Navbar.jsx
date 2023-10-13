f9df01dc2cee8d02fc2c9d6e72069d28ecac44e3
zeredogo <zeredogo@gmail.com>
2023-10-13 23:21:34 +0000

    change commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,9 @@ import { AiOutlineClose } from 'react-icons/ai';
 import logo from '../../images/logo.png';
 const Navbar = () => {
     return (
-        <h1>Navbar</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            f
+        </nav>
     );
 }
 
