d849d5b26f290a3653ca2cb32457457dd6c92bc7
zeredogo <zeredogo@gmail.com>
2023-10-15 19:36:00 +0100

    update commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
               <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
                 {[ "Market", "Exchange", "Tutorials", "Wallets"].map((item, index) =>(
                     < NavbarItem key={item + index} title={item}/>
-                ))}
+                ))} 
                 <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
                     Login
                 </li>
@@ -34,7 +34,8 @@ const Navbar = () => {
                   : <HiMenuAlt4 fontsize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
                 }
                 {toggleMenu && (
-                    <ul>
+                    <ul className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2x1 md:hidden list-none
+                     flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'>
                         <li className='text-xl w-full my-2'>
                             <AiOutlineClose onClick={() => setToggleMenu(false)} />
                         </li>
