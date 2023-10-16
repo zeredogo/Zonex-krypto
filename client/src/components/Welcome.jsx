24e89d1bbee3ec06fdb94dda373a6c492aeb7f7d
zeredogo <zeredogo@gmail.com>
2023-10-16 23:23:54 +0100

    update commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -8,7 +8,11 @@ const Welcome = () => {
     return (
         <div className="flex w-full justify-center item-center">
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start flex-col md:mr-10"></div>
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1>
+                        Send Crypto <br /> across the world
+                    </h1>
+                </div>
             </div>
         </div>
     );
