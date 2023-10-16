0c780792ee2eaf7ab463df879713afea9e8cd24a
zeredogo <zeredogo@gmail.com>
2023-10-16 20:46:19 +0100

    update commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,5 +1,8 @@
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from "./";
 
 const Welcome = () => {
     return (
