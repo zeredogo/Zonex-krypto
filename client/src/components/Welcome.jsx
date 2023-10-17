f05612c3c35039f94eb80b73ea3dc41427c92ce9
zeredogo <zeredogo@gmail.com>
2023-10-18 00:09:43 +0100

    update commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from "./";
 
-const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border=[0.5px] border-gray-400 text"
+const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
 
 const Welcome = () => {
 
