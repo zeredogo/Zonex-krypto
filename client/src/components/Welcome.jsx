d55214efbd29b38a60402ad51675337e6dfb4c08
zeredogo <zeredogo@gmail.com>
2023-10-18 00:35:32 +0100

    update commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -29,7 +29,31 @@ const Welcome = () => {
                         </p>
                     </button>
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-                        <div className={`rounded-tl-2xl ${commonStyles}`}></div>
+                        <div className={`rounded-tl-2xl ${commonStyles}`}>
+                            Reliability
+                        </div>
+                        <div className={commonStyles}>Security</div>
+                        <div className={`rounded-tr-2xl ${commonStyles}`}>
+                            Ethereum
+                        </div>
+                        <div className={`rounded-bl-2xl ${commonStyles}`}>
+                            Wed 3.0
+                        </div>
+                        <div className={commonStyles}>Low fees</div>
+                        <div className={`rounded-br-2xl ${commonStyles}`}>
+                            Blockchain
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 my-10">
+                    <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
+                        <div className="flex justify-between flex-col w-full h-full">
+                            <div className="flex justify-between items-start">
+                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                                    <SiEthereum fontSize={21} color="#fff" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
