75ef1fef03be099791835bd6c8f410cd96fa24cf
zeredogo <zeredogo@gmail.com>
2023-10-17 23:29:24 +0100

    update commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -9,9 +9,12 @@ const Welcome = () => {
         <div className="flex w-full justify-center item-center">
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
                 <div className="flex flex-1 justify-start flex-col md:mr-10">
-                    <h1>
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
                         Send Crypto <br /> across the world
                     </h1>
+                    <p>
+                        Explore the crypto world. Buy and sell cryptocurrencies easily on krypto.
+                    </p>
                 </div>
             </div>
         </div>
