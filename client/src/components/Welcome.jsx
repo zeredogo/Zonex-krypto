aec1fab02dd10a2fd19aa2f623b74cb33d798fb0
zeredogo <zeredogo@gmail.com>
2023-10-18 22:15:09 +0100

    update commit

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -88,7 +88,7 @@ const Welcome = () => {
                         <button
                         type="button"
                         onClick={handleSubmit}
-                        className="text-white w-full mt-2 border-[1px] p2 border[#3d4f7c]"
+                        className="text-white w-full mt-2 border-[1px] p2 border[#3d4f7c] rounded-full cursor-pointer"
                         >
                             Send Now
                         </button>
