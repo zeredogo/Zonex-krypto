2f1b108358f8cc154934c87ddcbd6b3b73c264f9
zeredogo <zeredogo@gmail.com>
2023-10-21 22:23:58 +0100

    update commit

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
     return (
         <div className="flex justify-center items-center py-3">
-            <div className="animate-spin rounded-full"></div>
+            <div className="animate-spin rounded-full h-32"></div>
         </div>
     );
 }
